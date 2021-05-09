77b78613278c0f80188fa6f878265a392a80dfec
Ian Lambert <lambe347@umn.edu>
2021-05-09 15:33:21 -0500

    server post request function setup to send from client to database, now to finish the client side of this request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const pool = require('./modules/pool');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -9,6 +11,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
+app.post('/feedback', (req, res) => {
+    let newFeedback = req.body;
+    console.log(`Adding feedback to database`, newFeedback);
+
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                    VALUES ($1, $2, $3, $4)l;`;
+
+    pool.query(queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log(`Error with adding new feedback to database`, error);
+            res.sendStatus(500);
+        });
+});
 
 
 /** ---------- START SERVER ---------- **/
